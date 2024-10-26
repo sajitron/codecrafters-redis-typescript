33bc963f483be5abdbea7d5650a258b443f52e1a
Adams Banjo <sajioloye@gmail.com>
2024-10-26 16:53:25 -0400

    implement SET & GET

--- app/main.ts
+++ app/main.ts
@@ -13,10 +13,25 @@ const server: net.Server = net.createServer((connection: net.Socket) => {
 //
 server.listen(6379, "127.0.0.1");
 
+const storage: Record<string, string> = {};
+
 function getValue(data: string): string {
-  const [, , key, , arg] = data.split("\r\n");
-  if (key === "ECHO") {
-    return `$${arg.length}\r\n${arg}\r\n`;
+  const [, , cmd, , key, , value] = data.split("\r\n");
+  if (cmd.toLowerCase() === "echo") {
+    return parseResponse(key);
+  } else if (cmd.toLowerCase() === "set") {
+    storage[key] = value;
+    return `+OK\r\n`;
+  } else if (cmd.toLowerCase() === "get") {
+    const value = storage[key];
+    if (!value) {
+      return "$-1\r\n";
+    }
+    return parseResponse(value);
   }
   return `+PONG\r\n`;
+}
+
+function parseResponse(value: string): string {
+  return `$${value.length}\r\n${value}\r\n`;
 }
