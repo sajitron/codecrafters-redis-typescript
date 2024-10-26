f8859263a7582f607c69212c587b9d21b92b6678
Adams Banjo <sajioloye@gmail.com>
2024-10-26 16:30:03 -0400

    implement echo command

--- app/main.ts
+++ app/main.ts
@@ -6,8 +6,17 @@ console.log("Logs from your program will appear here!");
 const server: net.Server = net.createServer((connection: net.Socket) => {
   // Handle connection
   connection.on("data", (data) => {
-    connection.write("+PONG\r\n");
+    const value = getValue(data.toString());
+    connection.write(value);
   });
 });
 //
 server.listen(6379, "127.0.0.1");
+
+function getValue(data: string): string {
+  const [, , key, , arg] = data.split("\r\n");
+  if (key === "ECHO") {
+    return `$${arg.length}\r\n${arg}\r\n`;
+  }
+  return `+PONG\r\n`;
+}
